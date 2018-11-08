f42cedf7f30d21e5bbafee6ab5cfaf59ce90bfc7
iiMostafaYT <43861336+iiMostafaYT@users.noreply.github.com>
2018-11-08 20:40:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,32 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-const fs = require('fs');
+
+const Util = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
 const moment = require('moment');
+
 const jimp = require('jimp');
+
 const Canvas = require('canvas');
-const prefix = '#'
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
+
+const client = new Discord.Client({disableEveryone: true});
+
+const prefix = "#";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
