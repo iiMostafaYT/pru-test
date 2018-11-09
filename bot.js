896ceb67540319d3a0b0580e4f7b006226055018
iiMostafaYT <43861336+iiMostafaYT@users.noreply.github.com>
2018-11-09 12:49:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1216,45 +1216,6 @@ client.on('message', message => {
 });
 
 
-var dat = JSON.parse(fs.readFileSync('./invite.json', 'utf8'));
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("510176527447490576")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'test');
-    if (!channel) {
-        console.log("!channel fails");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('made it till here!');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("510176527447490576")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
-                    console.log(3);
- channel.send(`${member} Joined By ${Invite.inviter}`)
- }
-            dat[Inv] = Invite.uses;
-        })
-    })
-});
+
 
 client.login(process.env.BOT_TOKEN);
