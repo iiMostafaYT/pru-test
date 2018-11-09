b921d5d06fc717637e024e7d4c2734464b10c03d
iiMostafaYT <43861336+iiMostafaYT@users.noreply.github.com>
2018-11-09 12:15:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -391,7 +391,7 @@ client.on("message", message => {
       
        :rocket: البوت يعمل 24 ساعه
       
-       :free: البوت مجاني %100
+   
 `)
    message.author.sendEmbed(embed)
     
@@ -408,7 +408,7 @@ client.on("message", message => {
       .setDescription(`
       __**Administrative Commands**__
     **  『#move @user / لسحب الشخص الى روومك』
-      『3bc / رسالة جماعية الى كل اعضاء السيرفر』
+      『#bc / رسالة جماعية الى كل اعضاء السيرفر』
       『#hchannel / اخفاء الشات』
       『#schannel / اضهار الشات المخفية』
       『#clear / مسح الشات』
@@ -1215,57 +1215,6 @@ client.on('message', message => {
        }
 });
 
-const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
-
-const coolDown = new Set();
-
-client.on('message',async message => {
-    
-if(message.author.bot) return;
-if(!credits[message.author.id]) credits[message.author.id] = {
-    credits: 50
-};
-
-let userData = credits[message.author.id];
-let m = userData.credits;
-
-fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
-    if (err) console.error(err);
-  });
-  credits[message.author.id] = {
-      credits: m + 0.5,
-  }
-  
-    if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
-message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`);
-}
-});
-
-client.on('message', async message => {
-    let amount = 250;
-    if(message.content.startsWith(prefix + "daily")) {
-    if(message.author.bot) return;
-    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
-    
-    let userData = credits[message.author.id];
-    let m = userData.credits + amount;
-    credits[message.author.id] = {
-    credits: m
-    };
-
-    fs.writeFile("./creditsCode.json", JSON.stringify(userData.credits + amount), (err) => {
-    if (err) console.error(err);
-    });
-    
-    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
-        coolDown.add(message.author.id);
-    });
-    
-    setTimeout(() => {
-       coolDown.remove(message.author.id);
-    },86400000);
-    }
-});
 
 var dat = JSON.parse(fs.readFileSync('./invite.json', 'utf8'));
 function forEachObject(obj, func) {
